bb410b63e9cb81f6ca45e875d6d0f9e1879a7bfa
Qubot <1445788683@qq.com>
2021-01-29 16:40:15 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -17,6 +17,8 @@ namespace qcar {
     let LastTime:number;
     let LeftSpeed:number;
     let RightSpeed:number;
+    LeftMotor=0;
+    RightMotor=0
 
     export enum Patrol {
         //% blockId="patrolLeft" block="left"
@@ -116,8 +118,6 @@ namespace qcar {
    //% blockId=Motor_Speed block="read |%speed Motor Speed"
    //% patrol.fieldEditor="gridpicker" patrol.fieldOptions.columns=2 
     export function Motor_Speed(speed: Speed): number {
-        LeftMotor=0;
-        RightMotor=0
         if (speed == Speed.LeftSpeed) {
             if (LastTime < 1000) {
                 if ((pins.digitalReadPin(DigitalPin.P5)==1)&&(LeftMotor==0)) {
