04f978ae70f18741432e070ba4969b7dfb13a667
Qubot <1445788683@qq.com>
2021-03-01 15:38:41 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -316,30 +316,26 @@ namespace qcar {
    export function QCar_Direction(Car_Direction: Direction): void {
     if (Car_Direction == Direction.foward) {
 
-        // Low byte of onStep
         write(64, 0x06, 0 & 0xFF)
-    
-        // High byte of onStep
         write(64, 0x07, (0 >> 8) & 0x0F)
-    
-        // Low byte of offStep
         write(64, 0x08, 4095 & 0xFF)
-    
-        // High byte of offStep
         write(64, 0x09, (4095 >> 8) & 0x0F)
             
-    
-        // Low byte of onStep
         write(64, 0x0A, 4095 & 0xFF)
-    
-        // High byte of onStep
         write(64, 0x0B, (4095 >> 8) & 0x0F)
-    
-        // Low byte of offStep
         write(64, 0x0C, 0 & 0xFF)
-    
-        // High byte of offStep
         write(64, 0x0D, (0 >> 8) & 0x0F)
+
+
+        write(64, 0x0E, 0 & 0xFF)
+        write(64, 0x0E, (0 >> 8) & 0x0F)
+        write(64, 0x10, 4095 & 0xFF)
+        write(64, 0x11, (4095 >> 8) & 0x0F)
+
+        write(64, 0x12, 4095 & 0xFF)
+        write(64, 0x13, (4095 >> 8) & 0x0F)
+        write(64, 0x14, 0 & 0xFF)
+        write(64, 0x15, (0 >> 8) & 0x0F)
     } 
     else if (Car_Direction == Direction.back) {
     } 
