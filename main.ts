1aaba82994ff1c5bee899a98df0f6e87dd5cafb4
Qubot <1445788683@qq.com>
2021-03-08 15:02:33 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -28,6 +28,16 @@ namespace qcar {
         return (25000000 / (freq * chipResolution)) - 1;
     }
 
+    function stripHexPrefix(str: string): string {
+        if (str.length === 2) {
+            return str
+        }
+        if (str.substr(0, 2) === '0x') {
+            return str.substr(-2, 2)
+        }
+        return str
+    }
+
     export enum Patrol {
         //% blockId="patrolLeft" block="left"
         PatrolLeft = 2,
